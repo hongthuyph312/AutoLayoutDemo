9dad8b6431c193ef6cc490588d51b98aa5ecd6e9
ThuyPH <hong.thuy@more-com.co.jp>
2016-11-03 11:37:40 +0700

    no message

--- AutoLayoutDemo/AutoLayoutDemo/TestVisualLanguageController.m
+++ AutoLayoutDemo/AutoLayoutDemo/TestVisualLanguageController.m
@@ -36,18 +36,22 @@
     [greenView setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.view addSubview:greenView];
     
-    // Đối với Label có thể không cần set with heigh cho đối tượng
+    // Đối với Label có thể không cần set with height cho đối tượng
     UILabel *lbCenter = [[UILabel alloc] init];
-    lbCenter.text = @"Đối với Label có thể không cần set with heigh cho đối tượng";
+    lbCenter.text = @"Đối với Label có thể không cần set with height cho đối tượng, nhưng chữ sẽ bị tràn ra ngoài view. Khi xét width thì label sẽ tự xuống dòng để vừa với text cần hiển thị mà không cần phải set height cho label";
     lbCenter.textAlignment = NSTextAlignmentCenter;
-    lbCenter.backgroundColor = [UIColor redColor];
+    lbCenter.backgroundColor = [UIColor lightGrayColor];
     lbCenter.translatesAutoresizingMaskIntoConstraints = NO;
+    lbCenter.font = [UIFont systemFontOfSize:14];
+    lbCenter.numberOfLines = 0;
     [self.view addSubview:lbCenter];
     
     //VFL String
     NSDictionary *dict = @{@"redView":redView,
                            @"blueView":blueView,
-                           @"greenView":greenView};
+                           @"greenView":greenView,
+                           @"lbCenter":lbCenter,
+                           @"parentView":self.view};
 
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-[redView(==greenView)]-[blueView(==greenView)]-[greenView(>=50)]-|"
                                                                       options:NSLayoutFormatAlignAllCenterY
@@ -66,12 +70,18 @@
                                                                         metrics:nil
                                                                         views:dict]];
     
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[lbCenter]"
-                                                                      options:NSLayoutFormatAlignAllCenterY
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[lbCenter(200)]"
+                                                                      options:0
                                                                       metrics:nil
-                                                                        views:@{@"lbCenter":lbCenter}]];
-    
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[lbCenter]" options:NSLayoutFormatAlignAllCenterX metrics:nil views:@{@"lbCenter":lbCenter}]];
+                                                                        views:dict]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[parentView]-(<=1)-[lbCenter]"
+                                                                      options:NSLayoutFormatAlignAllCenterX
+                                                                      metrics:nil
+                                                                        views:dict]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[redView]-20-[lbCenter]"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:dict]];
 }
 
 - (void)didReceiveMemoryWarning {
